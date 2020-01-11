cf841d22828ef1495d8ca28eca81e9128fafcb5a
prubac <prubac@sgh.waw.pl>
2020-01-11 13:23:10 +0100

    Stuktura Bank i praca domowa 4

--- Bank.py
+++ Bank.py
@@ -17,12 +17,31 @@ class Account:
         self.customer = customer
         self.balance = 0
 
+    def uznanie(self, kwota):
+        self.balance += kwota
+
+    def obciazenie(self, kwota):
+        if kwota > self.balance:
+            raise BankException('Brak srodkow na obciazenie, jest tylko {}'.format(self.balance))
+        self.balance -= kwota
+
     def __str__(self):
         return "Account[{0},{1},{2}]".format(self.account_id, self.customer.nazwisko, self.balance)
+
+class BankException(Exception):
+    pass
+
 
 c1 = Customer('Jan', 'Kowalski')
 c2 = Customer('Anna', 'Malinowska')
 a1 = Account(c1)
-a2 = Account(c2)
-print(a1)
-print(a2)
+try:
+    a1.uznanie(100)
+    a1.obciazenie(160)
+    a2 = Account(c2)
+    print(a1)
+    print(a2)
+except BankException as be:
+    print(be)
+
+
